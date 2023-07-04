e48a4bccd0564b6495ba3d04a61ad5f6f7523282
Uzair Iqbal <uzair.qq@outlook.com>
2023-07-04 06:47:49 -0700

    Backend EmployeesGetListAsync And PostASync

---
+++ Fullstack.Backend/FullStack.WebApi/FullStack.WebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,36 @@
+﻿using FullStack.WebApi.Data;
+using FullStack.WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly FullStackDbContext _fullStackDbContext;
+        public EmployeesController(FullStackDbContext fullStackDbContext)
+        {
+            _fullStackDbContext = fullStackDbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetListAsync()
+        {
+            var result = await _fullStackDbContext.Employees.ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Employee employee)
+        {
+            employee.Id = new Guid();
+            await _fullStackDbContext.Employees.AddAsync(employee);
+            await _fullStackDbContext.SaveChangesAsync();
+            return Ok(employee);
+        }
+    }
+}
