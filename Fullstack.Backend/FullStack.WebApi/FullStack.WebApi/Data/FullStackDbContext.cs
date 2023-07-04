9d93aeab6b7ed3872e64261a3b2569f2c4a47942
Uzair Iqbal <uzair.qq@outlook.com>
2023-07-04 06:20:29 -0700

    Added Employee Model, DbContext, Installed EfCore, Efcore.SqlServer and EfCore.Tools And Also With Conntection Stirng

---
+++ Fullstack.Backend/FullStack.WebApi/FullStack.WebApi/Data/FullStackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FullStack.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.WebApi.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Fullstack.Backend/FullStack.WebApi/FullStack.WebApi/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace FullStack.WebApi.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public int Salary { get; set; }
+        public string Department { get; set; }
+    }
+}
+
+
+//id: string;
+//firstName: string;
+//lastName: string;
+//email: string;
+//phoneNo: number;
+//salary: number;
+//department: string;
