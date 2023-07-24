d1dca6d9e987b54ff32641a649c32b13269f593a
Uzair Iqbal <uzair.qq@outlook.com>
2023-07-24 11:13:05 -0700

    FrontEnd End Going To Particular Id Record By View Click Using Router Link.

--- Fullstack.UI/src/app/app-routing.module.ts
+++ Fullstack.UI/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './components/employees/employee-list/employee-list.component';
 import { AddEmployeeComponent } from './components/employees/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/employees/edit-employee/edit-employee.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path: 'employees/add',
     component: AddEmployeeComponent,
+  },
+  {
+    path: 'employees/edit/:id',
+    component: EditEmployeeComponent,
   },
 ];
 
